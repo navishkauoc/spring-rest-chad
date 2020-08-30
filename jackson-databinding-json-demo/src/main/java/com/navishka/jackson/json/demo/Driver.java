10339d3a62d934733751d348d70994611b95b25c
Navishka Kularathna <navishka.randika@gmail.com>
2020-08-30 21:19:25 +0530

    JSON data binding with Jackson

---
+++ jackson-databinding-json-demo/src/main/java/com/navishka/jackson/json/demo/Driver.java
@@ -0,0 +1,29 @@
+package com.navishka.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			//Create object mapper
+			ObjectMapper mapper = new ObjectMapper();
+			
+			//Read JSON file and map/convert to Java POJO: data/sample-lite.json
+			Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			
+			//Print first name and last name
+			System.out.println("First name: " + theStudent.getFirstName());
+			System.out.println("Last name: " + theStudent.getLastName());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
