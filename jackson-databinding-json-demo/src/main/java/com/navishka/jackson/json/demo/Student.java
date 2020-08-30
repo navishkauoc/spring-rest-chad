44f1305554f2bd809d2430e4a90545c50e6f1350
Navishka Kularathna <navishka.randika@gmail.com>
2020-08-30 22:52:25 +0530

    JSON Ignore properties annotation added

--- jackson-databinding-json-demo/src/main/java/com/navishka/jackson/json/demo/Student.java
+++ jackson-databinding-json-demo/src/main/java/com/navishka/jackson/json/demo/Student.java
@@ -1,5 +1,8 @@
 package com.navishka.jackson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class Student {
 	
 	private int id;
